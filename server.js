acb0c3af7b4d61a375102683a6406f19caa88282
svfreitas <svfreitas@yahoo.com>
2018-06-08 12:48:49 -0300

    commit 6

--- server.js
+++ server.js
@@ -15,4 +15,5 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 
 
-app.listen(3000, () => console.log('Listening on port 3000!'))
+//app.listen(3000, () => console.log('Listening on port 3000!'))
+app.listen(3000);
