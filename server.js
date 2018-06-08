cf3a8f7eecfaa0844dc0c540131e81e2dbd5141d
svfreitas <svfreitas@yahoo.com>
2018-06-08 16:06:01 -0300

    kkk

--- server.js
+++ server.js
@@ -7,16 +7,17 @@ var app = express();
  app.use((req,res,next)=>{
         var filename = __dirname+'/log.txt';
         var data = new Date().toString() + ' ' + req.url +'\n';
-console.log(filename);
-console.log(data);
-         fs.appendFile(filename,data,(err) => {
+        console.log(filename);
+        console.log(data);
+        
+        fs.appendFile(filename,data,(err) => {
             if (err) throw err;
            // console.log('The "data to append" was appended to file!');
-          })  
-         next();
+        });
+        next();
  });
 
- app.get('/', (req, res) => res.send('<html><body><h1>Testando</h1></body></html>'))
+app.get('/', (req, res) => res.send('<html><body><h1>Testando</h1></body></html>'))
 app.get('/hello', (req, res) => res.send('<html><body><h1>Hello World!!!!</h1></body></html>'))
 
 
