c3139ae18d372c7637535e9b29c5a80142bec7e4
svfreitas <svfreitas@yahoo.com>
2018-06-08 11:46:24 -0300

    Init commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const fs = require('fs');
+
+var app = express();
+
+
+app.use((req,res,next)=>{
+    fs.appendFileSync('log.txt',new Date().toString() + ' ' + req.url,(err) => {});
+    next();
+});
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
