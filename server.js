a5355896dac6e6836d05da4479e300def422192f
svfreitas <svfreitas@yahoo.com>
2018-06-08 12:12:16 -0300

    commit port 80

--- server.js
+++ server.js
@@ -15,4 +15,4 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 
 
-app.listen(3000, () => console.log('Listening on port 3000!'))
+app.listen(80, () => console.log('Listening on port 80!'))
