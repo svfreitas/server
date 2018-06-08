4ead080a42070bdd7ae61c7ab196f1d0cb1e5571
svfreitas <svfreitas@yahoo.com>
2018-06-08 11:51:37 -0300

    server.js change

--- server.js
+++ server.js
@@ -12,4 +12,4 @@ app.use((req,res,next)=>{
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Listening on port 3000!'))
