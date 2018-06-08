6d9390d32b2eb77a8f720779b54d47c060a57778
svfreitas <svfreitas@yahoo.com>
2018-06-08 13:21:51 -0300

    commit 8

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const fs = require('fs');
 var app = express();
 
 
-// app.use((req,res,next)=>{
-//     fs.appendFileSync('log.txt',new Date().toString() + ' ' + req.url,(err) => {});
-//     next();
-// });
+ app.use((req,res,next)=>{
+         fs.appendFileSync('log.txt',new Date().toString() + ' ' + req.url,(err) => {});  
+         next();
+ });
 
 
-app.get('/teste', (req, res) => res.send('<html><body><h1>Hello World!</h1></body></html>'))
+app.get('/hello', (req, res) => res.send('<html><body><h1>Hello World!</h1></body></html>'))
 
 
 
