fb3966c1befe71f872b11ae07307bed42244654a
svfreitas <svfreitas@yahoo.com>
2018-06-11 13:38:04 -0300

    restify

--- server.js
+++ server.js
@@ -1,29 +1,27 @@
 const express = require('express');
 const fs = require('fs');
+const rest = require(restify);
 
-var app = express();
+//var app = express();
 
 
- app.use((req,res,next)=>{
-        var filename = __dirname+'/log.txt';
-        var data = new Date().toString() + ' ' + req.url +'\n';
-        console.log(filename);
-        console.log(data);
-        
-        // fs.appendFile(filename,data,(err) => {
-        //     if (err) throw err;
-        //    // console.log('The "data to append" was appended to file!');
-        // });
+var server = restify.createServer();
+
+function respond(req, res, next) {
+    res.send('hello ' + req.params.name);
+    next();
+  }
+  
+  var server = restify.createServer();
+  server.get('/hello/:name', respond);
+  server.head('/hello/:name', respond);
+  
+  
+  var port = process.env.PORT || 3000;
+  server.listen(port, function() {
+    console.log('%s listening at %s', server.name, server.url);
+  });
  
-        next();
- });
 
-app.get('/', (req, res) => res.send('<html><body><h1>Testando 123456</h1></body></html>'))
-app.get('/hello!', (req, res) => res.send('<html><body><h1>Hello World!!!!</h1></body></html>'))
-
-
-
-var port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}!`));
 
 
